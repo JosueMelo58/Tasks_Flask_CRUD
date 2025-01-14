96b355c63ee23888ed18511c9fb9b14af91764a7
JosueMelo58 <140197714+JosueMelo58@users.noreply.github.com>
2025-01-14 14:26:46 -0300

    Add app.py & requirements.txt

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#definir a rota raiz (Página inicial) e a função que será executada ao ser requisitada
+
+
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
